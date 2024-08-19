82225c34a0d9f6dfa31c24711c60382c5b0b4742
ryu10 <ryu10nt@gmail.com>
2024-08-18 19:16:57 -0700

    working with auto-height

--- art_scad_gen.py
+++ art_scad_gen.py
@@ -12,16 +12,16 @@ def get_options(input_file):
 
 ### main
 
-# colormap = {
-#     "white":    ("white", "white", 0),
-#     "black":    ("black", "black", 1),
-#     "dark_blue":  ("black", "blue", 2),
-#     "light_blie":   ("white", "yellow", 3),
-#     "red":      ("cyan", "magenta",1),
-#     "green":    ("cyan", "yellow", 2),
-#     "blue":     ("magenta", "cyan", 3),
-#     "orange":   ("magenta", "yellow", 4),
-# }
+colormap = {
+    "white":    ("white", "white", 0),
+    "black":    ("black", "black", 1),
+    "dark_blue":  ("black", "blue", 3),
+    "light_blue":   ("white", "blue", 2),
+    "red":      ("cyan", "magenta",1),
+    "green":    ("cyan", "yellow", 2),
+    "blue":     ("magenta", "cyan", 3),
+    "orange":   ("magenta", "yellow", 4),
+}
 
 l = 3; # size of base
 
@@ -37,21 +37,7 @@ with open(args.input_file) as f:
     shape_info = tetrisdata_whole["shape_info"]
 
 # represent 8 colors with a combination of two plate colors (in the order of lowwer, upper)
-colortable = {
-    "white":    ("white", "white"),
-    "black":    ("black", "black"),
-    "dark_blue":  ("black", "blue"),
-    "light_blie":   ("white", "yellow"),
-    "red":      ("cyan", "magenta"),
-    "green":    ("cyan", "yellow"),
-    "blue":     ("magenta", "cyan"),
-    "orange":   ("magenta", "yellow"),
-    "black":    ("white", "white") # this is an exception, represet black with white 
-}
-# add more colors
-colortable["black"] = ("black", "black")
-colortable["light_blue"] = ("white", "blue")
-colortable["dark_blue"] = ("black", "blue")
+colortable = colormap
 
 # map cell index to color name
 index_to_colorname = {}
@@ -75,6 +61,7 @@ l2=l*0.9;  // size of top plates
 t=0.2002;  // thickness of top plates
 r=l/2;   // for base fillets
 d=2;     // for base fillets
+hunit=2; // layers per height
 
 ''' % (l))
 
@@ -92,17 +79,17 @@ translate([%d, %d, 0])
 union(){
     if(%s==true){
         color("%s")
-        tetris_base();
+        tetris_base(%d);
     }
 
     if(%s==true){
         color("%s")
-        tetris_plate(0);
+        tetris_plate(0, %d);
     }
 
     if(%s==true){
         color("%s")
-        tetris_plate(1);
+        tetris_plate(1, %d);
     }
 }
 '''
@@ -119,22 +106,24 @@ for y in range(y_max):
         colorindex = tetrisdata[tetrisdata_index]
         colorname = index_to_colorname[colorindex]
         tetrisdata_index = tetrisdata_index + 1
+        x = x_max - x # reverse
         print("// X: %d, Y: %d, Colorname: %s" % (x, y, colorname))
         color_plate0 = colortable[colorname][0]
         color_plate1 = colortable[colorname][1]
+        height = colortable[colorname][2]
         print(script_block % (x*l, y*l, 
-                              "tetris_white", "white",
-                              "tetris_"+color_plate0, color_plate0,
-                              "tetris_"+color_plate1, color_plate1)); 
+                              "tetris_white", "white", height,
+                              "tetris_"+color_plate0, color_plate0, height,
+                              "tetris_"+color_plate1, color_plate1, height)); 
 
 # print module definition tetris_base (l:size, d:height, r:fillet_radius)
 print('''
-module tetris_base(){
+module tetris_base(h1){
     intersection() {
         translate([0,0,0])
-            cube([l, l, d]);
+            cube([l, l, d+h1*hunit]);
         translate([r, r, -d*0.01]){
-            cylinder(h=d*1.02, d=r*1.6/sin(atan(1/sqrt(2)))-r/22);
+            cylinder(h=(d+h1*hunit)*1.02, d=r*1.6/sin(atan(1/sqrt(2)))-r/22);
         }
     }    
 }
@@ -143,9 +132,9 @@ module tetris_base(){
 # print module definition tetris_plate (l:base size, l2: plate size, d:height)
 #   (arg level = plate height (0, 1, 2, ...))
 print('''
-module tetris_plate(level){
+module tetris_plate(level, h1){
     o1=(l-l2)/2;
-    translate([o1,o1,d+t*level])
+    translate([o1,o1,d+h1*hunit+level*t])
         cube([l2, l2, t]);
 }
 ''')
