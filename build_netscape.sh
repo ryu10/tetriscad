1829b339168f4adf6fa9610df8a9a7ee5572ec5e
ryu10 <ryu10nt@gmail.com>
2024-09-05 18:53:53 -0700

    fix typos

--- build_netscape.sh
+++ build_netscape.sh
@@ -11,4 +11,4 @@ rm -rf build/netscape
 mkdir -p build/netscape
 openscad -o build/netscape/ns_art_white.stl netscape.scad -D tetris_white=true  -D tetris_black=false -D tetris_blue=false
 openscad -o build/netscape/ns_art_black.stl netscape.scad -D tetris_white=false -D tetris_black=true  -D tetris_blue=false
-openscad -o build/netscape/ns_art_bkue.stl  netscape.scad -D tetris_white=false -D tetris_black=false -D tetris_blue=true
+openscad -o build/netscape/ns_art_blue.stl  netscape.scad -D tetris_white=false -D tetris_black=false -D tetris_blue=true
